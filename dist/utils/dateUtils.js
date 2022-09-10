8f74b2c42f372f74e83db97a611d4b2460529cc9
Yinlin Chen <ylchen@vt.edu>
2022-09-10 17:06:06 -0400

    update readme

--- dist/utils/dateUtils.js
+++ dist/utils/dateUtils.js
@@ -6,8 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.datesToDue = void 0;
 const moment_1 = __importDefault(require("moment"));
 const datesToDue = (date) => {
-    const eventDate = (0, moment_1.default)(date).utc();
-    const today = (0, moment_1.default)().utc();
-    return eventDate.diff(today, 'days');
+    const eventDate = (0, moment_1.default)(date).local();
+    const today = (0, moment_1.default)().local();
+    const diffMinutes = eventDate.diff(today, 'minutes');
+    if (diffMinutes > 10080)
+        return 8;
+    else if (diffMinutes <= 10080 && diffMinutes > 4320)
+        return 7;
+    else if (diffMinutes <= 4320 && diffMinutes > 1440)
+        return 3;
+    else if (diffMinutes <= 1440 && diffMinutes > 0)
+        return 1;
+    else
+        return -1;
 };
 exports.datesToDue = datesToDue;
--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,7 +1,20 @@
 import moment from 'moment';
 
 export const datesToDue = (date: string) => {
-  const eventDate = moment(date).utc();
-  const today = moment().utc();
-  return eventDate.diff(today, 'days');
+  const eventDate = moment(date).local();
+  const today = moment().local();
+
+  const diffMinutes = eventDate.diff(today, 'minutes')
+
+  if (diffMinutes > 10080)
+    return 8
+  else if (diffMinutes <= 10080 && diffMinutes > 4320)
+    return 7
+  else if (diffMinutes <= 4320 && diffMinutes > 1440)
+    return 3
+  else if (diffMinutes <= 1440 && diffMinutes > 0)
+    return 1
+  else
+    return -1
+
 };
