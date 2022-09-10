f6f364a2a6ebccb6e63b8ed44817bd098e869fe3
Yinlin Chen <ylchen@vt.edu>
2022-09-10 12:14:26 -0400

    customize

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 import {context} from '@actions/github';
 import Octokit from './integrations/Octokit';
 import {datesToDue} from './utils/dateUtils';
-import {OVERDUE_TAG_NAME, NEXT_WEEK_TAG_NAME} from './constants';
+import {OVERDUE_TAG_NAME, DUE_THREEDAYS_TAG_NAME, NEXT_WEEK_TAG_NAME, DUE_TOMORROW_TAG_NAME} from './constants';
 
 export const run = async () => {
   try {
@@ -18,13 +18,23 @@ export const run = async () => {
     for (const issue of results) {
       const daysUtilDueDate = await datesToDue(issue.due);
 
-      // Between 0 and 7 days until due date
-      if (daysUtilDueDate <= 7 && daysUtilDueDate > 0) {
+      // 7 days until due date
+      if (daysUtilDueDate == 7 && daysUtilDueDate > 0) {
         await ok.addLabelToIssue(context.repo.owner, context.repo.repo, issue.number, [NEXT_WEEK_TAG_NAME]);
+      }
+      // 3 days due
+      if (daysUtilDueDate == 3 && daysUtilDueDate > 0) {
+        await ok.removeLabelFromIssue(context.repo.owner, context.repo.repo, NEXT_WEEK_TAG_NAME, issue.number);
+        await ok.addLabelToIssue(context.repo.owner, context.repo.repo, issue.number, [DUE_THREEDAYS_TAG_NAME]);
+      }
+      // 1 day due
+      if (daysUtilDueDate == 1 && daysUtilDueDate > 0) {
+        await ok.removeLabelFromIssue(context.repo.owner, context.repo.repo, DUE_THREEDAYS_TAG_NAME, issue.number);
+        await ok.addLabelToIssue(context.repo.owner, context.repo.repo, issue.number, [DUE_TOMORROW_TAG_NAME]);
       }
       // Issue is due
       if (daysUtilDueDate <= 0) {
-        await ok.removeLabelFromIssue(context.repo.owner, context.repo.repo, NEXT_WEEK_TAG_NAME, issue.number);
+        await ok.removeLabelFromIssue(context.repo.owner, context.repo.repo, DUE_TOMORROW_TAG_NAME, issue.number);
         await ok.addLabelToIssue(context.repo.owner, context.repo.repo, issue.number, [OVERDUE_TAG_NAME]);
       }
     }
--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
 export const OVERDUE_TAG_NAME: string = 'Overdue';
 export const NEXT_WEEK_TAG_NAME: string = 'Due next week';
+export const DUE_THREEDAYS_TAG_NAME: string = 'Due in 3 days';
+export const DUE_TOMORROW_TAG_NAME: string = 'Due tomorrow';
