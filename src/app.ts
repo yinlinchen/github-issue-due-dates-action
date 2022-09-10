27cf9d3571e7e744c52b996fb13e4c6f614cc944
Yinlin Chen <ylchen@vt.edu>
2022-09-10 13:51:13 -0400

    adjustmet

--- src/app.ts
+++ src/app.ts
@@ -19,11 +19,11 @@ export const run = async () => {
       const daysUtilDueDate = await datesToDue(issue.due);
 
       // 7 days until due date
-      if (daysUtilDueDate <= 7 && daysUtilDueDate > 0) {
+      if (daysUtilDueDate <= 7 && daysUtilDueDate > 3) {
         await ok.addLabelToIssue(context.repo.owner, context.repo.repo, issue.number, [NEXT_WEEK_TAG_NAME]);
       }
       // 3 days due
-      if (daysUtilDueDate <= 3 && daysUtilDueDate > 0) {
+      if (daysUtilDueDate <= 3 && daysUtilDueDate > 1) {
         await ok.removeLabelFromIssue(context.repo.owner, context.repo.repo, NEXT_WEEK_TAG_NAME, issue.number);
         await ok.addLabelToIssue(context.repo.owner, context.repo.repo, issue.number, [DUE_THREEDAYS_TAG_NAME]);
       }
